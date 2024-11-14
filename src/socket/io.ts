70d3a5b9487283c31d4112b0357528d93d13a22d
Moshe <m0504136536@gmail.com>
2024-11-14 17:38:30 +0200

    socket alarm done

--- src/socket/io.ts
+++ src/socket/io.ts
@@ -1,8 +1,13 @@
 import { Socket } from "socket.io";
+import { io } from "../app";
 
 export const startConnection = (socket: Socket) => {
     console.log(`a user connected ${socket.id}`);
     socket.on('disconnect', () => {
         console.log('Bye bye user');
+    })
+
+    socket.on('dispatch', (data) => {
+        socket.broadcast.emit('alarm',data);
     })
 }
